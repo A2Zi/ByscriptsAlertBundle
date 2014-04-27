f6420ff7537bde7e1f1023febe8d35b1e99fcd49
Thierry Goettelmann <thierry@byscripts.info>
2014-04-27 15:57:00 +0200

    Fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -38,6 +38,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
             ->arrayNode('classes')
+            ->children()
             ->append($this->iconsNode())
             ->append($this->typesNode());
 
